ccd953f45bdaf5c441c08f50f0215765e4525eba
Calrance Kho <calrancedev@gmail.com>
2025-12-12 11:24:40 +0800

    feat: add dayjs

--- src/app/pages/notes/notes.ts
+++ src/app/pages/notes/notes.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './notes.html',
 })
 export class Notes {
-
+  noteTitle = 'Getting Started';
 }
